cb0b001dbf12903d8c8581684bb19ad89695d8ca
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-17 12:01:32 +0500

    Add license Gate files in rollupOptions

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,42 +3,38 @@ import { bytecodePlugin, defineConfig, externalizeDepsPlugin } from 'electron-vi
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
-    // build: {
-    //   rollupOptions: {
-    //     input: {
-    //       app: resolve(__dirname, 'src/main/index.ts'),
-    //       licenseGate: resolve(__dirname, 'src/main/licenseGate.ts')
-    //     }
-    //   }
-    // }
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
-    // build: {
-    //   rollupOptions: {
-    //     input: {
-    //       app: resolve(__dirname, 'src/preload/index.ts'),
-    //       licenseGate: resolve(__dirname, 'src/preload/licenseGate.ts')
-    //     }
-    //   }
-    // }
-  },
-  renderer: {
-    // build: {
-    //   rollupOptions: {
-    //     input: {
-    //       app: resolve(__dirname, 'src/renderer/index.html'),
-    //       licenseGate: resolve(__dirname, 'src/renderer/licenseGate.html')
-    //     }
-    //   }
-    // },
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    main: {
+        build: {
+            sourcemap: true,
+        },
+        plugins: [externalizeDepsPlugin(), bytecodePlugin()],
     },
-    plugins: [react()]
-  }
+    preload: {
+        plugins: [externalizeDepsPlugin(), bytecodePlugin()],
+        build: {
+            rollupOptions: {
+                input: {
+                    app: resolve(__dirname, 'src/preload/index.ts'),
+                    licenseGate: resolve(__dirname, 'src/preload/licenseGate.ts'),
+                },
+            },
+        },
+    },
+    renderer: {
+        build: {
+            sourcemap: true,
+            rollupOptions: {
+                input: {
+                    app: resolve(__dirname, 'src/renderer/index.html'),
+                    licenseGate: resolve(__dirname, 'src/renderer/licenseGate.html'),
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                '@renderer': resolve('src/renderer/src'),
+            },
+        },
+        plugins: [react()],
+    },
 })
