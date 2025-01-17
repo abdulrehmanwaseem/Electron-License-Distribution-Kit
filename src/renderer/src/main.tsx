359df75354838d93aeb2a4e061f116675b930c30
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-18 00:41:41 +0500

    Some refactors

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Topbar from './components/Topbar'
-import MacTopbar from './components/MacTopbar'
+// import MacTopbar from './components/MacTopbar'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
