30e473a68c7e48317efe8300f33bb995a4600236
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-19 14:04:25 +0500

    Refactor sentry plugin config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,32 +3,32 @@ import { bytecodePlugin, defineConfig, externalizeDepsPlugin } from 'electron-vi
 import react from '@vitejs/plugin-react'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
+// Sentry plugin configuration for error tracking and release management
 const sentryPluginConfig = {
     org: 'electron-license-distribution-kit',
     project: 'desktop',
-    ...(import.meta.env.SENTRY_AUTH_TOKEN
-        ? {
-              authToken: import.meta.env.MAIN_VITE_SENTRY_AUTH_TOKEN, // Use token if provided
-              release: import.meta.env.MAIN_VITE_RELEASE || '1.0.0', // Optional release version
-              include: './dist',
-              sourcemaps: {
-                  assets: './**/*',
-              },
-          }
-        : {}),
+
+    // Uncomment and set these options when preparing for distribution, otherwise will get a warning when building:
+
+    // authToken: import.meta.env.MAIN_VITE_SENTRY_AUTH_TOKEN, // Use token if provided
+    // release: import.meta.env.MAIN_VITE_RELEASE || '1.0.0', // Optional release version
+    // include: './dist',
+    // sourcemaps: {
+    //     assets: './**/*',
+    // },
 }
 
 export default defineConfig({
     main: {
         build: {
-            sourcemap: true,
+            sourcemap: false, // Enable if you need more detailed error reports from SentryVitePlugin.
         },
         plugins: [externalizeDepsPlugin(), sentryVitePlugin(sentryPluginConfig), bytecodePlugin()],
     },
     preload: {
         plugins: [externalizeDepsPlugin(), sentryVitePlugin(sentryPluginConfig), bytecodePlugin()],
         build: {
-            sourcemap: true,
+            sourcemap: false, // Enable if you need more detailed error reports from SentryVitePlugin.
             rollupOptions: {
                 input: {
                     index: resolve(__dirname, 'src/preload/index.ts'),
@@ -39,7 +39,7 @@ export default defineConfig({
     },
     renderer: {
         build: {
-            sourcemap: true,
+            sourcemap: false, // Enable if you need more detailed error reports from SentryVitePlugin.
             rollupOptions: {
                 input: {
                     index: resolve(__dirname, 'src/renderer/index.html'),
